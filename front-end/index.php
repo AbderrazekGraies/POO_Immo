4584313daf541ff7dd04768d31944586438a51f0
unknown <alib.38@outlook.fr>
2023-01-03 15:36:34 +0100

    test

---
+++ front-end/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="img-logo">
+            <img src="../assetes/img/logo-img.png" height="50px" alt="">
+        </div>
+        <h1>Chez vous</h1>
+        <input type="search" placeholder="rercher votre maison">
+        <img src="../assetes/img/avatar.png" height="50px" alt="">
+
+    </nav>
+</body>
+
+</html>

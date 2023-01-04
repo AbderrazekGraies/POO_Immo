f1d7e36db9f23dd1cbcf924357c6aa605977f9ac
unknown <alib.38@outlook.fr>
2023-01-04 10:07:56 +0100

    add main paage

--- front-end/index.php
+++ front-end/index.php
@@ -23,7 +23,19 @@
     </nav>
 
     <header>
+        <div class="container">
+            <div class="img-product">
+                <img src="../assetes/img/first-img.webp" height="250px" alt="">
+            </div>
+            <div>
+                <p>Isere, Rhones alpes</p>
+                <p>Belle maison avec vue sur mer</p>
+                <p>Disponible</p>
+                <p>Prix: 100£</p>
 
+
+            </div>
+        </div>
     </header>
 
 
